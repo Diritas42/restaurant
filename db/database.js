372277659e40e983cc77054fb62eff01cb73b5a2
Diritas42 <s.d.trabo@students.psu.by>
2026-05-27 11:25:07 +0300

    obnova

--- db/database.js
+++ db/database.js
@@ -2,11 +2,15 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { rejectUnauthorized: false },
+    max: 5,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
 });
 
 pool.on('error', (err) => {
     console.error('Неожиданная ошибка PostgreSQL', err);
 });
 
+// Важно: в serverless среде следует кэшировать пул глобально
 module.exports = { pool };
